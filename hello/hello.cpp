241314001d97ba97deacf88ac31d93c773d32e2a
chacha <chafree93@gmail.com>
2023-01-09 16:07:13 +0900

    20220109.01

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <array>
+
 using namespace std;
 
 int main(void)
 {
+    array<int, 3> arr={9, 8, 7};
+
+    cout << "Array size = " << arr.size() << endl;
+    for(auto i : arr) cout << i << endl;
+
     cout << "Hello World!" << endl;
 
     return 0;
