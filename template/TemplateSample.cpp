5a1f57d9e22e64ce320b25e5b51dfebe0071d5fa
chacha <chafree93@gmail.com>
2023-01-12 23:57:01 +0900

    20230112.03

---
+++ template/TemplateSample.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class CMyData
+{
+public:
+	CMyData(T param):m_Data(param){}
+	T GetData() const {return m_Data;}
+
+	operator T(){return m_Data;}
+	void SetData(T param){m_Data=param;}
+
+private:
+	T m_Data;
+};
+
+int main()
+{
+	CMyData<int> a(5);
+	cout << a << endl;
+
+	CMyData<double> b(123.45);
+	cout << b << endl;
+
+	CMyData<char*> c("Hello");
+	cout << c << endl;
+	
+	return 0;
+}
+
+
